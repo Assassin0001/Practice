71ed2a4787081f405f4f06573f10870fc183f3b0
Aman Kumar <aman021276@gmail.com>
2022-06-07 15:04:53 +0530

    Update

--- Python/Q5.py
+++ Python/Q5.py
@@ -1,7 +1,13 @@
 r=[11,14,17,19,14,10,14,10,18,10,19,10,14,15,17,18,14,17,14]
+#Method 1
 key=list(set(r))
 val=[]
 for i in r:
     val.append(r.count(i))
 d1=dict(zip(key,val))
 print(d1)    
+
+#Method 2
+from collections import Counter
+res=Counter(r)
+print(res)
