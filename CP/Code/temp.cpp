deb36940961beb788a3bfb1daa9a6b8b6f912226
Aman Kumar <aman021276@gmail.com>
2023-01-13 19:18:13 +0530

    Practice File

--- CP/Code/temp.cpp
+++ CP/Code/temp.cpp
@@ -4,17 +4,7 @@ using namespace std;
 int main() {
 	int t; cin >> t;
 	while (t--) {
-		int n, y; cin >> n >> y;
-		int arr[n];
-		int sumor = 0;
-		for (int i = 0; i < n; i++) {
-			cin >> arr[i];
-			sumor = sumor | arr[i];
-		}
-		int ans = sumor ^ y;
-		if ((ans | sumor) == y)
-			cout << ans << endl;
-		else cout << -1 << endl;
+
 	}
 	return 0;
 }
