8a8ee7be6139ea522ee28e721735f53b2ef472ed
Aman Kumar <aman021276@gmail.com>
2023-01-19 16:39:25 +0530

    Practice File

--- CP/Code/temp.cpp
+++ CP/Code/temp.cpp
@@ -4,7 +4,29 @@ using namespace std;
 int main() {
 	int t; cin >> t;
 	while (t--) {
-
+		int n; cin >> n;
+		int* arr = new int[n];
+		for (int i = 0; i < n; i++) {
+			cin >> arr[i];
+		}
+		int cnt = 0;
+		bool ans = true;
+		for (int i = 0; i < n - 1; i++) {
+			if (arr[0] != arr[n - 1]) {
+				if (arr[i] != arr[i + 1]) cnt += 2;
+			}
+			else {
+				if (arr[i] != arr[i + 1]) {
+					cnt = cnt + 1;
+					ans = false;
+				}
+			}
+		}
+		if (arr[0] != arr[n - 1]) cout << cnt  << endl;
+		else {
+			if (ans) cout << 0 << endl;
+			else cout << cnt + 1 << endl;
+		}
 	}
 	return 0;
 }
