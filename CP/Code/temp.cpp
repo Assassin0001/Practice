80eeb0b970915d0caabea806f3c2751bc5414667
Aman Kumar <aman021276@gmail.com>
2023-05-18 03:32:18 +0530

    New FIle
    Practice File

--- CP/Code/temp.cpp
+++ CP/Code/temp.cpp
@@ -1,26 +1,58 @@
-#include <iostream>
-#include <algorithm>
+#include<bits/stdc++.h>
 using namespace std;
 
-int main() {
-	int n, a, b, c, d;
-	cin >> n >> a >> b >> c >> d;
+typedef long long int ll;
+typedef unordered_map<ll, ll> ml;
+typedef vector<ll> vl;
+typedef pair<ll, ll> pl;
+#define F first
+#define S second
+#define PB push_back
+#define POB pop_back
+#define MP make_pair
 
-	if (max(b, c) > 0) {
-		int flag1 = b - max(c, 0);
-		int flag2 = c - max(b , 0);
-		if (flag1 == 0 || flag1 == 1 || flag2 == 0 || flag2 == 1) {
-			cout << "Yes" << endl;
-		} else {
-			cout << "No" << endl;
+int main() {
+	//make input output fast
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+	int t; cin >> t;
+	while (t--) {
+		int n; cin >> n;
+		if (n == 3) {
+			ll a, b, c;
+			cin >> a >> b >> c;
+			if ((2 * b) == (a + c)) cout << "NO" << endl;
+			else cout << "YES" << endl;
 		}
-	} else {
-		if (a == 0 || d == 0) {
-			cout << "Yes" << endl;
-		} else {
-			cout << "No" << endl;
+		else {
+			vl v(n);
+			for (int i = 0; i < n; i++) {
+				cin >> v[i];
+			}
+
+			vl lSum;
+			ll sum = 0;
+			for (int i = 1; i < n; i++) {
+				sum += v[i] - v[i - 1];
+				lSum.PB(sum);
+			}
+
+			sum = 0;
+			vl rSum;
+			for (int i = n - 1; i > 0; i--) {
+				sum += v[i] - v[i - 1];
+				rSum.PB(sum);
+			}
+
+			reverse(rSum.begin(), rSum.end());
+			bool ans = false;
+			for (int i = 0; i < n - 1; i++) {
+				if (rSum[i] == lSum[i]) ans = true;
+			}
+
+			if (ans) cout << "NO" << endl;
+			else cout << "YES" << endl;
 		}
 	}
-
 	return 0;
 }
--- DSA/CodeHelp/Maths For DSA/CountPrime.cpp
+++ DSA/CodeHelp/Maths For DSA/CountPrime.cpp
@@ -1,18 +1,19 @@
-class Solution {
+class Solution
+{
 public:
-    int countPrimes(int n) {
-        int cnt = 0;
-        vector<bool> Prime(n+1,true);
-        Prime[0] = Prime[1] = false;
-
-        for(int i=2; i<n; i++){
-            if(Prime[i]){ 
-                cnt++;
-            for(int j = 2*i; j < n; j=j+i){
-                Prime[j] = 0;
-                } 
-            }   
+    vector<int> sieveOfEratosthenes(int N)
+    {
+        vector<int> ans;
+        for(int i=2; i<=N; i++){
+            bool flag = true;
+            for(int j=2; j*j<=i; j++){
+                if(i%j == 0){
+                   flag = false;
+                   break;
+                }
+            }
+            if(flag) ans.push_back(i);
         }
-        return cnt;
+        return ans;
     }
-}
+};
