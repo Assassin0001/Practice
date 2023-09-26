3d9f3d1bd89c5813baa94d884421b3166b649ba4
Aman Kumar <aman021276@gmail.com>
2023-09-26 06:30:19 +0530

    Added Routes and Form Methods

--- WebDev/Backend-6PP/index.js
+++ WebDev/Backend-6PP/index.js
@@ -1,10 +1,25 @@
-import express from "express";
+import express, { urlencoded } from "express";
 import path from "path";
 
 const app = express();
+const users = [];
+
+//Using middlewares
+app.use(express.static(path.join(path.resolve(), "public")));
+app.use(express.urlencoded({ extended: true }));
+
+//Setting up view engine
+app.set("view engine", "ejs");
 
 //Get Request
 app.get("/", (req, res) => {
+  //Static HTML :
+  // Shows error -> res.sendFile("./index.html");
+  // const pathLocation = path.resolve();
+  // res.sendFile(path.join(pathLocation,"./index.html"));
+
+  //Dynamic
+  res.render("index", { name: "Abhishek" });
   /*  
   res.sendStatus(404);
   res.json({
@@ -17,9 +32,22 @@ app.get("/", (req, res) => {
     
     success: true,
     products: [],
-  }); */
+  }); 
+  */
+});
 
-  res.sendFile("./index.html");
+app.post("/contact", (req, res) => {
+  console.log(req.body);
+  users.push({ userName: req.body.name, email: req.body.email });  
+  res.redirect("/success");
+});
+
+app.get("/success",(req,res)=>{
+  res.render("success");
+});
+
+app.get("/users",(req,res)=>{
+  res.json(users);
 });
 
 app.listen(5000, () => {
