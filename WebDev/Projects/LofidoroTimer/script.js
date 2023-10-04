b2d4b8166058b96434216247130142f2ec44ed52
Aman Kumar <aman021276@gmail.com>
2023-10-05 01:27:42 +0530

    Added Start/Pause Functionality

--- WebDev/Projects/LofidoroTimer/script.js
+++ WebDev/Projects/LofidoroTimer/script.js
@@ -1,14 +1,10 @@
 const timer = document.querySelector(".lofidoro-timer");
 const title = document.querySelector(".title");
 const startBtn = document.querySelector(".startBtn");
-const pauseBtn = document.querySelector(".pauseBtn");
-const resumeBtn = document.querySelector(".resumeBtn");
 const resetBtn = document.querySelector(".resetBtn");
-const pomoCountsDisplay = document.querySelector(".lofiCountsDisplay");
+const lofiCountsDisplay = document.querySelector(".lofiCountsDisplay");
 
-//making variables
 let WORK_TIME = 25 * 60;
-// const BREAK_TIME = 5 * 60;
 let timerID = null;
 let isBreak = false;
 let totalCount = 0;
@@ -16,11 +12,9 @@ let paused = false;
 let running = false;
 
 const updateTimer = () => {
-  const selectedRadio = document.querySelector(
-    'input[name="timerType"]:checked'
-  );
+  const selectedRadio = document.querySelector('input[name="timerType"]:checked');
   const timertext = selectedRadio ? `${selectedRadio.value}:00` : "25:00";
-  document.querySelector(".lofidoro-timer").innerHTML = timertext;
+  timer.innerHTML = timertext;
 
   WORK_TIME = Number(selectedRadio.value) * 60;
   if (WORK_TIME < 1500) isBreak = true;
@@ -34,110 +28,114 @@ document.querySelectorAll('input[name="timerType"]').forEach((radio) => {
   });
 });
 
-//Title Updation Function
 const updateTitle = (msg) => {
   title.textContent = msg;
 };
 
-//Function to save pomoCount to localStorage
+const updateButtonText = (text) => {
+  startBtn.textContent = text;
+};
+
 const saveLocalCounts = () => {
-  // let counts = JSON.parse(localStorage.getItem("LofI-doroCounts")) || 0; GPT
-  let counts = JSON.parse(localStorage.getItem("LofI-doroCounts"));
-  counts !== null ? counts++ : (counts = 1);
+  let counts = JSON.parse(localStorage.getItem("LofI-doroCounts")) || 0;
+  counts++;
   localStorage.setItem("LofI-doroCounts", JSON.stringify(counts));
 };
-//CountDown function
+
 const countDown = (time) => {
   return () => {
     running = true;
-    const mins = Math.floor(time / 60)
-      .toString()
-      .padStart(2, "0");
-    const secs = Math.floor(time % 60)
-      .toString()
-      .padStart(2, "0");
+    const mins = Math.floor(time / 60).toString().padStart(2, "0");
+    const secs = Math.floor(time % 60).toString().padStart(2, "0");
 
     timer.textContent = `${mins}:${secs}`;
     time--;
+
     if (time < 0) {
       running = false;
       stopTimer();
+
       if (isBreak) {
         isBreak = false;
         updateTitle("Completed Break Time.");
       } else {
         updateTitle("Completed a focus session");
-        setTimeout(
-          () => updateTitle("Start Timer again for next session"),
-          2000
-        );
+        setTimeout(() => updateTitle("Start Timer again for next session"), 2000);
         totalCount++;
         saveLocalCounts();
-        showPomoCounts();
+        showlofiCounts();
       }
     }
   };
 };
 
-//Arrow function for startTimer Button
 const startTimer = (startTime) => {
   if (timerID !== null) {
     stopTimer();
   }
+
   return setInterval(countDown(startTime), 1000);
 };
 
-//Arrow function for stopTimer Button
 const stopTimer = () => {
   clearInterval(timerID);
   timerID = null;
 };
 
-//Arrow function to get Time in seconds
 const getTimeInSeconds = (timeString) => {
   const [minutes, seconds] = timeString.split(":");
   return parseInt(minutes * 60) + parseInt(seconds);
 };
 
-//Arrow function to show Completed PomoCounts
-const showPomoCounts = () => {
+//
+const showlofiCounts = () => {
   let counts = JSON.parse(localStorage.getItem("LofIdoroCounts"));
+  
   if (counts > 0) {
-    pomoCountsDisplay.style.display = "flex";
+    lofiCountsDisplay.style.display = "flex";
   }
-  pomoCountsDisplay.firstElementChild.textContent = counts;
+
+  lofiCountsDisplay.firstElementChild.textContent = counts;
 };
 
-//Adding eventListener to startButton
+
+//Start-Pause Button 
 startBtn.addEventListener("click", () => {
   if (!running) {
     timerID = startTimer(WORK_TIME);
+    paused = false;
+    updateButtonText("Pause");
+    
     !isBreak
       ? updateTitle("Focus Mode Started")
       : updateTitle("Break Time Started");
+  } else {
+    if (!paused) {
+      stopTimer();
+      paused = true;
+      updateButtonText("Start");
+      updateTitle("Timer Paused");
+    } else {
+      const currentTime = getTimeInSeconds(timer.textContent);
+      timerID = startTimer(currentTime);
+      paused = false;
+      updateButtonText("Pause");
+      !isBreak ? updateTitle("It's Work Time") : updateTitle("It's LoFI-Time");
+    }
   }
 });
 
-//Adding eventListener to resetButton
-resetBtn.addEventListener("click", () => {
+//Reset Button
+resetBtn.addEventListener("click",function() {
+  //Animations
+  this.style.animation = 'none'; 
+  this.offsetHeight; 
+  this.style.animation = null; 
+  this.classList.add('rotate');
+
+  //Logic For Reset Button
   (paused = false), (running = false);
+  updateButtonText("Start");
   stopTimer();
   updateTimer();
-});
-
-//Adding eventListener to pauseButton
-pauseBtn.addEventListener("click", () => {
-  stopTimer();
-  paused = true;
-  updateTitle("Timer Paused");
-});
-
-//Adding eventListener to resumeButton
-resumeBtn.addEventListener("click", () => {
-  if (paused) {
-    const currentTime = getTimeInSeconds(timer.textContent);
-    timerID = startTimer(currentTime);
-    paused = false;
-    !isBreak ? updateTitle("It's Work Time") : updateTitle("It's LoFI-Time");
-  }
 });
