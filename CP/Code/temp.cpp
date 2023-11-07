05882f71f9ed6329d3bd9b85bc7800b2579d9b02
Aman Kumar <aman021276@gmail.com>
2023-11-07 15:01:12 +0530

    Strict Checking

--- CP/Code/temp.cpp
+++ CP/Code/temp.cpp
@@ -4,39 +4,39 @@ using namespace std;
 #define ll long long int
 #define ull unsigned long long int
 #define PB push_back
-#define yes cout<<"YES"<<endl
-#define no cout<<"NO"<<endl
+#define yes cout<<"Yes"<<endl
+#define no cout<<"No"<<endl
 #define f first
 #define s second
 #define read(x) ll x;cin>>x
 #define show(x) cout<<x<<endl
+#define vl vector<long long>
 #define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL)
 
 void solve() {
-	read(P); read(Q);
+	read(n); read(k);
+	string a; cin >> a;
 
-	vector<int> first(P);
-	for (int i = 0; i < P; i++) {
-		cin >> first[i];
+	unordered_map<char, int> freq;
+	for (auto it : a) {	freq[it]++; }
+	int disnct = 0;
+	for (auto it : freq) {
+		if (it.second == 1) disnct++;
 	}
-
-	vector<int> second(Q);
-	for (int i = 0; i < Q; i++) {
-		cin >> days[i];
+	int len = n - k;
+	if (len % 2) {
+		if (k % 2)	disnct <= k + 1 ? yes : no;
+		else disnct <= k + 1 ? yes : no;
 	}
-	sort(first.begin(), first.end());
-	sort(second.begin(), second.end());
-	int j = 0;
-	for (int i = 1; i <= N; i++) {
-		if (i > days[j]) {j++;}
-		cout << days[j] - i << endl;
+	else {
+		if (k % 2) disnct <= k + 1 ? yes : no;
+		else disnct <= k ? yes : no;
 	}
 }
 
 int main() {
 	fast_io;
-	int t; //cin >> t;
-	t = 1;
+	int t; cin >> t;
 	while (t--) {
 		solve();
 	}
