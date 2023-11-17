ff5d44aa91a05683d0a281060572c5e48daeeb0b
Aman Kumar <aman021276@gmail.com>
2023-11-18 04:06:20 +0530

    SignUp Fixed

--- WebDev/Projects/fitness-tracker/src/app/auth/signup/signup.component.ts
+++ WebDev/Projects/fitness-tracker/src/app/auth/signup/signup.component.ts
@@ -20,7 +20,7 @@ export class SignupComponent implements OnInit {
   onSubmit(form: NgForm) {
     this.authService.registerUser({
       email: form.value.email,
-      password: form.value.passwdm
+      password: form.value.passwd,
     });
   }
 }
